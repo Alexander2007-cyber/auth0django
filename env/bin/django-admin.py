31c0a2d164a3668e5f32c262d8ca5549b546985f
Alexander Yordanov <alex123yordanov@gmail.com>
2021-01-10 12:26:22 +0200

    init

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/alexander/Documents/code/auth0/django-01-login/01-login/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
